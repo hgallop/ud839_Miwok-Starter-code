1b6ccc9e223661ecda399cd7a37544e8af7aa797
Heather Gallop <doyouseeitmyway@gmail.com>
2018-05-05 16:34:25 -0400

    Change ConstraintLayout to LinearLayout in activity_numbers. Delete log messages. Add id to the LinearLayout. Dynamically generate three TextViews. Set the TextViews to display the first three items of the ArrayList. Add the TextViews to the Layout using java.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -26,16 +28,25 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("eight");
         words.add("nine");
         words.add("ten");
-        //log array list contents
-        Log.v("words 0", words.get(0) + "");
-        Log.v("words 1", words.get(1) + "");
-        Log.v("words 2", words.get(2) + "");
-        Log.v("words 3", words.get(3) + "");
-        Log.v("words 4", words.get(4) + "");
-        Log.v("words 5", words.get(5) + "");
-        Log.v("words 6", words.get(6)+ "");
-        Log.v("words 7", words.get(7) + "");
-        Log.v("words 8", words.get(8) + "");
-        Log.v("words 9", words.get(9) + "");
+        //finds the linear layout in the view hierarchy
+        LinearLayout rootView = findViewById(R.id.rootView);
+        //creates a new text view
+        TextView wordView = new TextView(this);
+        //sets text of the text view to the contents of the array list at index 0
+        wordView.setText(words.get(0));
+        //adds text view to the linear layout
+        rootView.addView(wordView);
+        //creates a new text view
+        TextView wordView2 = new TextView(this);
+        //sets text of the text view to the contents of the array list at index 1
+        wordView2.setText(words.get(1));
+        //adds text view to the linear layout
+        rootView.addView(wordView2);
+        //creates a new text view
+        TextView wordView3 = new TextView(this);
+        //sets text of the text view to the contents of the array list at index 2
+        wordView3.setText(words.get(2));
+        //adds text view to the linear layout
+        rootView.addView(wordView3);
     }
 }
