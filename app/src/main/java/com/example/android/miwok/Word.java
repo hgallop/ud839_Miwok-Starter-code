3964c70ddecb4877dcff4039c1f6cf2b6675b8b7
Heather Gallop <doyouseeitmyway@gmail.com>
2018-06-02 09:23:00 -0400

    Update list item layout to have an image view. Update the res with drawables for every density, Update the Word class to have a member variable and an additional constructor that holds/has an image resource, Update Colors, Numbers, and Family Member Activities Array list of objects with the proper image resource.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -7,11 +7,19 @@ public class Word {
     //variables for miwok and default words
     private String mMiwokTranslation;
     private String mDefaultTranslation;
+    private int mListImage;
 
     //constructor for the object
     Word(String def, String miwok) {
         mMiwokTranslation = miwok;
         mDefaultTranslation = def;
+    }
+
+    //constructor for the object
+    Word(String def, String miwok, int image) {
+        mMiwokTranslation = miwok;
+        mDefaultTranslation = def;
+        mListImage = image;
     }
 
     //getter method for miwok word
@@ -22,5 +30,9 @@ public class Word {
     //getter method for default word
     public String getDefaultTranslation() {
         return mDefaultTranslation;
+    }
+
+    public int getListImage() {
+        return mListImage;
     }
 }
