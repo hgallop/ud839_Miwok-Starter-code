c36534e9cf761661e42a29b8c93a16cacd779d35
Heather Gallop <doyouseeitmyway@gmail.com>
2018-06-09 11:12:01 -0400

    Update Word class to accept a resource id for an audio file. Update each category's words ArrayList of Word objects with the audio file resource id. Update each category class with an OnItemClickListener that creates a MediaPlayer object and sets it to play the audio file for the current Word object from the ArrayList.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.miwok;
 
+import android.app.Activity;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import java.util.ArrayList;
 
@@ -13,18 +17,18 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.word_list);
 
         //create new array list object
-        ArrayList<Word> words = new ArrayList<>();
+        final ArrayList<Word> words = new ArrayList<>();
         //add word objects to array list
-        words.add(new Word("one", "lutti", R.drawable.number_one));
-        words.add(new Word("two","otiiko", R.drawable.number_two));
-        words.add(new Word("three","tolookosu", R.drawable.number_three));
-        words.add(new Word("four","oyyisa", R.drawable.number_four));
-        words.add(new Word("five", "massokka", R.drawable.number_five));
-        words.add(new Word("six", "temmokka", R.drawable.number_six));
-        words.add(new Word("seven", "kenekaku", R.drawable.number_seven));
-        words.add(new Word("eight", "kawinta", R.drawable.number_eight));
-        words.add(new Word("nine", "wo'e", R.drawable.number_nine));
-        words.add(new Word("ten", "na'aacha", R.drawable.number_ten));
+        words.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
+        words.add(new Word("two","otiiko", R.drawable.number_two, R.raw.number_two));
+        words.add(new Word("three","tolookosu", R.drawable.number_three, R.raw.number_three));
+        words.add(new Word("four","oyyisa", R.drawable.number_four, R.raw.number_four));
+        words.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
+        words.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
+        words.add(new Word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
+        words.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
+        words.add(new Word("nine", "wo'e", R.drawable.number_nine, R.raw.number_nine));
+        words.add(new Word("ten", "na'aacha", R.drawable.number_ten, R.raw.number_ten));
         //finds the list view in the view hierarchy
         ListView rootView = findViewById(R.id.list);
         //creates an word adapter object to iterate through the array list and set each item to a layout holding two text views
@@ -32,5 +36,12 @@ public class NumbersActivity extends AppCompatActivity {
         //set the word adapter object to the list view
         rootView.setAdapter(wordAdapter);
 
+        rootView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                MediaPlayer wordPlayer = MediaPlayer.create(NumbersActivity.this, words.get(position).getAudio());
+                wordPlayer.start();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -9,19 +9,22 @@ public class Word {
     //variables for miwok and default words
     private String mMiwokTranslation;
     private String mDefaultTranslation;
+    private int mAudio;
     private int mListImage = NO_IMAGE_PROVIDED;
 
     //constructor for the object
-    Word(String def, String miwok) {
+    Word(String def, String miwok, int audio) {
         mMiwokTranslation = miwok;
         mDefaultTranslation = def;
+        mAudio = audio;
     }
 
     //constructor for the object
-    Word(String def, String miwok, int image) {
+    Word(String def, String miwok, int image, int audio) {
         mMiwokTranslation = miwok;
         mDefaultTranslation = def;
         mListImage = image;
+        mAudio = audio;
     }
 
     //getter method for miwok word
@@ -36,6 +39,10 @@ public class Word {
 
     public int getListImage() {
         return mListImage;
+    }
+
+    public int getAudio() {
+        return mAudio;
     }
 
     public boolean hasImage() {
